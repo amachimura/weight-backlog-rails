16097544e56e14a15df9209c2079a24b1d40fcb8
diodario <machimura.akira@gmail.com>
2016-08-10 15:17:19 +0900

    created usermodel

---
+++ weight-backlog/test/models/user_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = User.new(name:"sampleUser", email:"user@example.com",
+     weight:85, secure_password: "hogehoge")
+  end
+
+  test "validity" do
+    assert @user.valid?
+  end
+
+  test "email format validation" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org]
+  end
+
+
+end
