af9098034b8ccdb3a14d1c877dca1c98db712f35
diodario <machimura.akira@gmail.com>
2016-08-10 15:17:42 +0900

    created usermodel

--- weight-backlog/app/models/user.rb
+++ weight-backlog/app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence:true, length: {maximum: 50}, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
+  validates :email, presence:true, length: {maximum: 50},
+  format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
   validates :weight, length: {maximum: 5}
+  validates :password, length: {minimum: 6}
   has_secure_password
 end
