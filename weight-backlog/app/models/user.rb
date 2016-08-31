5a09eccf8614794d6483edc284a841a678b4f8f0
diodario <machimura.akira@gmail.com>
2016-08-31 15:46:12 +0900

    make API for get User

--- weight-backlog/app/models/user.rb
+++ weight-backlog/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   validates :email, presence:true, length: {maximum: 50},
   format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
   validates :weight, length: {maximum: 5}
-  validates :password, length: {minimum: 6}
   has_secure_password
+  validates :password, length: {minimum: 6}
 end
--- weight-backlog/db/schema.rb
+++ weight-backlog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810063640) do
+ActiveRecord::Schema.define(version: 20160830083022) do
 
   create_table "daily_weights", force: :cascade do |t|
     t.integer  "user_id"
@@ -52,8 +52,9 @@ ActiveRecord::Schema.define(version: 20160810063640) do
     t.string   "name"
     t.string   "email"
     t.float    "weight"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
